55730ca246a2b7d4633f78ea804c06937423317c
Aleksei Latyshev <alex_700_95@mail.ru>
2018-05-31 14:26:47 +0300

    init

---
+++ src/test/java/Test.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b;
+        if (a == 0) {
+            b = in.nextInt();
+        } else {
+            b = in.nextInt();
+        }
+        System.out.println(a + b);
+    }
+}
