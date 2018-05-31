dec34ec53b8a63790143cbcc21429c0b068e0870
Aleksei Latyshev <alex_700_95@mail.ru>
2018-05-31 17:21:50 +0300

    add some cli

--- src/main/java/ru/ifmo/ctddev/latyshev/analyzer/Main.java
+++ src/main/java/ru/ifmo/ctddev/latyshev/analyzer/Main.java
@@ -5,6 +5,10 @@ import com.github.javaparser.symbolsolver.model.resolution.TypeSolver;
 import com.github.javaparser.symbolsolver.resolution.typesolvers.CombinedTypeSolver;
 import com.github.javaparser.symbolsolver.resolution.typesolvers.JavaParserTypeSolver;
 import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver;
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.ArgumentParserException;
+import net.sourceforge.argparse4j.inf.Namespace;
 
 import java.io.IOException;
 import java.nio.file.*;
@@ -12,14 +16,28 @@ import java.nio.file.attribute.BasicFileAttributes;
 import java.util.List;
 
 public class Main {
-    private static Path src = Paths.get("src/test/java");
-    private static TypeSolver typeSolver = new CombinedTypeSolver(
-        new ReflectionTypeSolver(),
-        new JavaParserTypeSolver(src)
-    );
-    private static JavaParserFacade parserFacade = JavaParserFacade.get(typeSolver);
-
     public static void main(String[] args) throws IOException {
+        ArgumentParser parser = ArgumentParsers.newFor("simple-java-analyzer").build()
+                .defaultHelp(true)
+                .description("Do static analyzes of given java project");
+        parser.addArgument("folder")
+                .required(true)
+                .help("Project folder to analyze.");
+        Namespace ns = null;
+        try {
+            ns = parser.parseArgs(args);
+        } catch (ArgumentParserException e) {
+            parser.handleError(e);
+            System.exit(1);
+        }
+
+        final Path src = Paths.get(ns.getString("folder"));
+        final TypeSolver typeSolver = new CombinedTypeSolver(
+                new ReflectionTypeSolver(),
+                new JavaParserTypeSolver(src)
+        );
+        final JavaParserFacade parserFacade = JavaParserFacade.get(typeSolver);
+
         SmellPrinter smellPrinter = new SimpleSmellPrinter();
         final List<Analyzer> analyzers = List.of(
             new ImportAnalyzer(smellPrinter),
